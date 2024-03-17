4cdeffb3b8b5d0f16d3248453fa2fafc56450f82
NoodleSamaChan <lferraraccio@outlook.com>
2024-03-17 11:23:28 +0100

    library for window creation

---
+++ src/lib.rs
@@ -0,0 +1,199 @@
+use std::fmt;
+use insta::assert_snapshot;
+
+#[derive(Debug)]
+pub struct WindowBuffer {
+    width: usize,
+    height: usize,
+    buffer: Vec<u32>,
+}
+
+impl WindowBuffer {
+    pub fn new(
+        width: usize,
+        height: usize,
+    ) -> Self {
+        Self {
+            width,
+            height,
+            buffer: vec![0; width * height],
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn buffer(&self) -> Vec<u32> {
+        self.buffer.clone()
+    }
+
+    pub fn get(&self, x: isize, y: isize) -> Option<u32> {
+        if (x >= 0) && ((x as usize) < self.width()) && (y >= 0) && ((y as usize) < self.height()) {
+            Some(self[(x as usize, y as usize)])
+        } else {
+            None
+        }
+    }
+
+}
+
+impl fmt::Display for WindowBuffer {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let line_len = self.buffer.chunks(self.width);
+        for i in line_len {
+            for a in i {
+                match a {
+                    0 => f.write_str(".")?,
+                    _ => f.write_str("#")?,
+                }
+            }
+            f.write_str("\n")?;
+        }
+        Ok(())
+    }
+}
+
+impl std::ops::Index<(usize, usize)> for WindowBuffer {
+    type Output = u32;
+
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        if x >= self.width {
+            panic!(
+                "Tried to index in a buffer of width {} with a x of {}",
+                self.width, x
+            );
+        }
+        if y >= self.height {
+            panic!(
+                "Tried to index in a buffer of height {} with a y of {}",
+                self.height, y
+            );
+        }
+
+        &self.buffer[y * self.width + x]
+    }
+}
+impl std::ops::IndexMut<(usize, usize)> for WindowBuffer {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        if x >= self.width {
+            panic!(
+                "Tried to index in a buffer of width {} with a x of {}",
+                self.width, x
+            );
+        }
+        if y >= self.height {
+            panic!(
+                "Tried to index in a buffer of height {} with a y of {}",
+                self.height, y
+            );
+        }
+
+        &mut self.buffer[y * self.width + x]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn display_window_buffer() {
+        let mut buffer = WindowBuffer::new(4, 4);
+        assert_eq!(
+            buffer.to_string(),
+            "....
+....
+....
+....
+"
+        );
+        buffer.buffer[1] = 1;
+        buffer.buffer[3] = 3;
+        buffer.buffer[4] = 4;
+        buffer.buffer[6] = 6;
+        buffer.buffer[9] = 9;
+        buffer.buffer[11] = 11;
+        buffer.buffer[12] = 12;
+        buffer.buffer[14] = 14;
+        assert_eq!(
+            buffer.to_string(),
+            ".#.#
+#.#.
+.#.#
+#.#.
+"
+        );
+    }
+
+    #[test]
+    fn display_window_buffer2() {
+        let mut buffer = WindowBuffer::new(4, 4);
+        assert_snapshot!(
+            buffer.to_string(),
+            @r###"
+        ....
+        ....
+        ....
+        ....
+        "###
+        );
+        buffer.buffer[1] = 1;
+        buffer.buffer[3] = 3;
+        buffer.buffer[4] = 4;
+        buffer.buffer[6] = 6;
+        buffer.buffer[9] = 9;
+        buffer.buffer[11] = 11;
+        buffer.buffer[12] = 12;
+        buffer.buffer[14] = 14;
+        assert_snapshot!(
+            buffer.to_string(),
+            @r###"
+        .#.#
+        #.#.
+        .#.#
+        #.#.
+        "###
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_index_width() {
+        let mut buffer = WindowBuffer::new(4, 4);
+        buffer[(0, 5)] = 0;
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_index_height() {
+        let mut buffer = WindowBuffer::new(4, 4);
+        buffer[(5, 0)] = 0;
+    }
+
+    #[test]
+    fn test_index() {
+        let mut buffer = WindowBuffer::new(4, 4);
+        buffer[(0, 1)] = 1;
+        buffer[(0, 3)] = 3;
+        buffer[(1, 0)] = 4;
+        buffer[(1, 2)] = 6;
+        buffer[(2, 1)] = 9;
+        buffer[(2, 3)] = 11;
+        buffer[(3, 0)] = 12;
+        buffer[(3, 2)] = 14;
+        assert_snapshot!(
+            buffer.to_string(),
+            @r###"
+        .#.#
+        #.#.
+        .#.#
+        #.#.
+        "###
+        );
+    }
+}
